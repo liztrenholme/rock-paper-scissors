f9bbfc2d0ee522c11ed28573ce7bd993b8e68bb4
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-03-10 18:41:29 -0500

    initial files

---
+++ assets/rps.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCmK6VgVTtzZ5ZDPOkGssOJLgMrK1mAdjI",
+    authDomain: "rock-paper-scissors-5c265.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-5c265.firebaseio.com",
+    projectId: "rock-paper-scissors-5c265",
+    storageBucket: "rock-paper-scissors-5c265.appspot.com",
+    messagingSenderId: "146335845104"
+  };
+  firebase.initializeApp(config);
