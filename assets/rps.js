6fe0a000c6a0952841b0ea7128d907a493d83298
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-03-12 11:08:54 -0400

    database connection established, name prompt working for player one

--- assets/rps.js
+++ assets/rps.js
@@ -8,3 +8,35 @@
     messagingSenderId: "146335845104"
   };
   firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var player1 = {
+    name: " ",
+    wins: 0,
+    losses: 0,
+    turn: false,
+    choice: " "
+  };
+
+var player2 = {
+    name: " ",
+    wins: 0,
+    losses: 0,
+    turn: false,
+    choice: " "
+  };
+
+  // On-click function for submitting a name.
+    $("button#submit-name").on("click", function () {
+        player1.name = $("#name-goes-here").val();
+        if (player1.name.length > 0) {
+              database.ref().set({
+                name: player1.name
+            });
+            $("#name-prompt").hide();
+            $(".headline").append("Hi, " + player1.name + "!  You are Player One!");
+        }
+
+        return false;
+    });
